7e13f1c86f428a357e1e8c442abc572bf0e5d32f
Sukrita <sugritabursuk@gmail.com>
2024-11-30 16:40:49 +0700

    lab6_1.cpp completed

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int integer;
+    cout << "Enter an integer: ";
+    cin >> integer ;
+
+    while(integer>0){
+        if(interger%2==0){
+            cin >> even num;
+        }{
+            if (interger%2!=0){
+                cin >> odd num;
+            }else{
+                cout << "#Even numbers = " << even num;
+                cout << "#Odd numbers = " << odd num;
+            }
+
+    return 0;
+}
